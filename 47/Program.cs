5da51ad36539ad4594c5800a902abda37c8cf330
KirilichevAO <kirilichev.a@icloud.com>
2022-09-02 10:05:28 +0700

    47

---
+++ 47/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5      7     -2   -0,2
+//   1   -3,3      8   -9,9
+//   8    7,8   -7,1      9
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+void CreateArrayDouble(double[,] array)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 9;
+        }
+    }
+}
+
+void PrintArrayDouble(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double smallNum = Math.Round(array[i, j], 1);
+            Console.Write(smallNum + " ");
+
+        }
+        Console.WriteLine("]");
+    }
+}
+
+CreateArrayDouble(array);
+
+PrintArrayDouble(array);
+
+Console.WriteLine();
